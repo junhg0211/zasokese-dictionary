b50bb8ef7d3c0abeebc8f8680f6b7c45eec3f565
Sch <junhg0211@gmail.com>
2023-02-15 16:09:42 +0900

    사전 불러오기 기능 구현

---
+++ __main__.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+from json import dump, load
+from json.decoder import JSONDecodeError
+from os import mkdir
+from os.path import isfile, isdir
+
+import gspread
+
+CREDENTIAL_PATH = './secret/credential.json'
+ZASOSPIKA_DICTIONARY = '1QSqIbmShJiUiJWNB0x8dQzGbb6W1dqEz_LBlP363e_E'
+
+
+class Dictionary:
+    def __init__(self):
+        self.path = './.dict_cache/zasospika.json'
+        self.data = None
+
+        if self.is_local_available():
+            try:
+                self.load()
+            except JSONDecodeError:
+                self.synchronize()
+        else:
+            self.synchronize()
+
+    def __str__(self):
+        return f'<Dictionary len(data)={len(self.data)}>'
+
+    def synchronize(self, fileout: bool = True):
+        """
+        구글 스프레드이시트에서 자소크어 사전을 불러온다.
+
+        그 후, ``self.data``를 동기화한다.
+        """
+        credential = gspread.service_account(filename=CREDENTIAL_PATH)
+        sheet = credential.open_by_key(ZASOSPIKA_DICTIONARY).get_worksheet(0)
+
+        self.data = sheet.get_all_values()[1:]
+
+        if fileout:
+            if not isdir('./.dict_cache'):
+                mkdir('./.dict_cache')
+            with open(self.path, 'w', encoding='utf-8') as file:
+                dump(self.data, file)
+
+    def is_local_available(self) -> bool:
+        """
+        ``load()`` 함수를 통해 불러올 수 있는 로컬 사전 저장 파일이 존재하는지 확인한다.
+        """
+        return isfile(self.path)
+
+    def load(self):
+        """
+        ``syncronize()`` 함수를 통해 로컬에 저장되어있는 함수를 불러온다.
+        """
+        with open(self.path, 'r', encoding='utf-8') as file:
+            self.data = load(file)
+
+
+def main():
+    dictionary = Dictionary()
+
+    print(dictionary)
+
+
+if __name__ == '__main__':
+    main()
